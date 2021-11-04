c972519a5d8d07ae43ce9f80b41f6749a308d2f1
Pauli Ojanperä <public@miettii.net>
2021-11-04 12:28:56 +0200

    Copy README.md into dist

--- post-build.mjs
+++ post-build.mjs
@@ -10,10 +10,13 @@ const log = () => {}; // console.log;
 
 (async () => {
 	const packageJson = path.join(__dirname, 'package.json');
+	const readme = path.join(__dirname, 'README.md');
 	const staticFiles = path.join(__dirname, 'static');
 	const dist = path.join(__dirname, 'dist');
 	log(`Copying files from ${staticFiles} to ${dist}.`);
 	log((fs.copySync(staticFiles, dist, { overwrite: true }) || {}).message || 'Done.');
-	log(`Copying files from ${packageJson} to ${dist}.`);
+	log(`Copying ${packageJson} to ${dist}.`);
 	log((fs.copySync(packageJson, path.join(dist, 'package.json'), { overwrite: true }) || {}).message || 'Done.');
+	log(`Copying ${readme} to ${dist}.`);
+	log((fs.copySync(readme, path.join(dist, 'README.md'), { overwrite: true }) || {}).message || 'Done.');
 })();
