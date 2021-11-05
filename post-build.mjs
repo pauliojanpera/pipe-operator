3d635b33bcde02db35d5149a9a8d4e359144e1a8
Pauli Ojanperä <public@miettii.net>
2021-11-05 17:18:10 +0200

    conf

--- post-build.mjs
+++ post-build.mjs
@@ -1,22 +1,16 @@
 // @ts-nocheck
 import path from 'path';
 import fs from 'fs-extra';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-// @ts-ignore
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = process.cwd();
 const log = () => {}; // console.log;
 
 (async () => {
-	const packageJson = path.join(__dirname, 'package.json');
-	const readme = path.join(__dirname, 'README.md');
-	const staticFiles = path.join(__dirname, 'static');
-	const dist = path.join(__dirname, 'dist');
-	log(`Copying files from ${staticFiles} to ${dist}.`);
-	log((fs.copySync(staticFiles, dist, { overwrite: true }) || {}).message || 'Done.');
-	log(`Copying ${packageJson} to ${dist}.`);
-	log((fs.copySync(packageJson, path.join(dist, 'package.json'), { overwrite: true }) || {}).message || 'Done.');
-	log(`Copying ${readme} to ${dist}.`);
-	log((fs.copySync(readme, path.join(dist, 'README.md'), { overwrite: true }) || {}).message || 'Done.');
+	for (const [src, dst] of ['package.json', 'README.md', '.npmrc', '.static'].map(fn => [
+		path.join(__dirname, fn),
+		path.join(__dirname, '.dist', fs.lstatSync(fn).isDirectory() ? '' : fn),
+	])) {
+		log(`Copying ${src} to ${dst}`);
+		log((fs.copySync(src, dst, { overwrite: true }) || {}).message || 'Done.');
+	}
 })();
